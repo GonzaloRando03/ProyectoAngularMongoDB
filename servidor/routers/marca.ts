e1026819f5a5db05ad8eb45e1198e4ab6b11032a
Gonzalo Rando Serna <103594582+GonzaloRando03@users.noreply.github.com>
2022-10-03 17:54:30 +0200

    DeleteOldBackend

--- servidor/routers/marca.ts
+++
@@ -1,8 +0,0 @@
-import * as express from "express"
-import { marcaController } from "../controllers/marcaController"
-
-export const routerMarca = express.Router()
-
-/* routers de las marcas */
-routerMarca.get('/', marcaController.getMarca)
-routerMarca.get('/:id', marcaController.getMarcaOne)
